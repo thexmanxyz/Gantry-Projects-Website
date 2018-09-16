beec7d15acff2040632e59f81f2580a3a694d865
thex <andreas.kar@gmx.at>
2018-09-16 23:12:59 +0200

    separated custom javascript

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+/******************************************************
+*                                                     *
+*   Gantry Projects Website                           *
+*                                                     *
+*   Purpose: This project contains the web resources  *
+*            for the Gantry Projects website.         *
+*                                                     *
+*   Author: Andreas Kar (thex) <andreas.kar@gmx.at>   *
+*   Repository: https://git.io/fA1FV                  *
+*   Homepage: https://gantryprojects.com              *
+*                                                     *
+******************************************************/
+
+$("body").toTopButton({clickSelectors: ['a.to-top-btm-button']});
